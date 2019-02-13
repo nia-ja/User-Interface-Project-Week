ef6191bd53fb5a04d19685df94ee6e58f96eabd5
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-02-13 12:10:31 -0800

    GLOBAL: Finished JS for the opening and closing functionality, linked the Home and Services pages together

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // JS goes here
+
+//NAVIGATION
+const toggleMenu = () => {
+    menu.classList.toggle('menu--open');
+    menuButtonClose.classList.remove('hamburger-close');
+    menuButton.classList.toggle('hamburger-close');
+}
+
+const closeMenu = () => {
+    menu.classList.remove('menu--open');
+    menuButton.classList.remove('hamburger-close');
+    menuButtonClose.classList.toggle('hamburger-close');
+}
+
+const menu = document.querySelector('.menu');
+const menuButton = document.querySelector('.hamburger-open');
+const menuButtonClose = document.querySelector('.hamburger-close');
+
+menuButton.addEventListener('click', toggleMenu);
+menuButtonClose.addEventListener('click', closeMenu); // <-- navigation.js
+
+
+
+// SERVICES PAGE - TAB COMPONENT
+
+
