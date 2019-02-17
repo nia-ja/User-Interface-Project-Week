05f7836d0afc349b94d6de84103e84b907b7017c
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-02-17 14:10:53 -0800

    ABOUT US: Finished building modal component (HTML, CSS, JS) for our team photo

---
+++ js/modal.js
@@ -0,0 +1,37 @@
+// // Modal component
+class TagName {
+    constructor(tagName) {
+        this.tagName = tagName;
+        this.data = this.tagName.dataset.name;
+        this.modalItem = document.querySelector(`.modal[data-name="${this.data}"]`);
+        this.modalItem = new ModalItem(this.modalItem);
+        this.tagName.addEventListener('click', this.openModal.bind(this));
+    };
+
+    openModal() {
+        this.modalItem.openModal();
+    }
+    closeModal() {
+        this.modalItem.closeModal();
+    }
+}
+
+class ModalItem {
+    constructor(modalItem) {
+        this.modalItem = modalItem;
+        this.closeBtn = this.modalItem.querySelector('.closeBtn');
+        console.log(this.closeBtn);
+        this.closeBtn.addEventListener('click', this.closeModal.bind(this));
+    }
+    openModal() {
+        this.modalItem.style.display = "block";
+
+    }
+    closeModal() {
+        this.modalItem.style.display = "none";
+    }
+}
+
+
+const names = document.querySelectorAll('.tag-name')
+                      .forEach(name => new TagName(name));
