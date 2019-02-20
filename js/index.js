3644cdf8801ef5e051efab1f73494c62948e023b
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-02-20 10:27:54 -0800

    GLOBAL: Changed GreenSock Animation for the menu appearance

--- js/index.js
+++ js/index.js
@@ -9,14 +9,16 @@ const openMenu = () => {
     menuButtonClose.classList.remove('hamburger-close');
     menuButton.classList.toggle('hamburger-close');
     // GreenSock animation for the menu appearance
-    TweenMax.from(menu, 2, {y:-1000, ease: Circ.easeOut});
+    TweenMax.from(menu, 2, {y:-1000, x:-300, ease: Circ.easeOut});
     TweenMax.to(menu, 2, {backgroundColor:"#98A2AE"});
     TweenMax.from(nav, 2, {backgroundColor:"rgba(152, 162, 174,0.1)", ease: Circ.easeOut});
+
+    TweenMax.from('a', 2, {opacity:0, x:-300});
 }
 
 const closeMenu = () => {
-    // GreenSock animation for the menu closing
-    TweenMax.to(nav, 1, {backgroundColor:"rgba(152, 162, 174,0.1)", y:-1000, ease: SlowMo.ease.config(0.1, 0.7, true)});
+    // GreenSock animation for the menu close
+    TweenMax.to(nav, 0.5, {backgroundColor:"rgba(152, 162, 174,0.1)", y:-50, ease: SlowMo.ease.config(0.5, 0.7, true)});
     menu.classList.remove('menu--open');
     menuButton.classList.remove('hamburger-close');
     menuButtonClose.classList.toggle('hamburger-close');
